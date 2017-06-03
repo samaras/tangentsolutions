5922532836c56204f0992ba45d5238083265b37b
samaras <skomfi@gmail.com>
2017-06-03 22:33:47 +0200

    employee signal

---
+++ hr/leave/signals.py
@@ -0,0 +1,10 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from 
+
+@receiver(post_save, sender=User)
+def create_employee(sender, created, instance, **kwargs):
+	if created:
+		employee = Employee(user=instance)
+		employee.save()
